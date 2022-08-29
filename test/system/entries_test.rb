f658ef4397e7ee451903b23e9fe03a1c3630d422
David Moina <davemolina777@gmail.com>
2022-08-29 11:30:53 -0500

    Create basic system tests

--- test/system/entries_test.rb
+++ test/system/entries_test.rb
@@ -7,23 +7,21 @@ class EntriesTest < ApplicationSystemTestCase
 
   test "visiting the index" do
     visit entries_url
-    assert_selector "h1", text: "Entries"
+    assert_selector "h1", text: "Food Log - #{DateTime.now.to_s(:nice_date)}"
   end
 
   test "creating a Entry" do
     visit entries_url
-    click_on "New Entry"
+    click_on "Create New Entry"
 
     fill_in "Calories", with: @entry.calories
     fill_in "Carbohydrates", with: @entry.carbohydrates
     fill_in "Fats", with: @entry.fats
-    fill_in "Integer", with: @entry.integer
     fill_in "Meal type", with: @entry.meal_type
     fill_in "Proteins", with: @entry.proteins
     click_on "Create Entry"
 
     assert_text "Entry was successfully created"
-    click_on "Back"
   end
 
   test "updating a Entry" do
@@ -33,13 +31,11 @@ class EntriesTest < ApplicationSystemTestCase
     fill_in "Calories", with: @entry.calories
     fill_in "Carbohydrates", with: @entry.carbohydrates
     fill_in "Fats", with: @entry.fats
-    fill_in "Integer", with: @entry.integer
     fill_in "Meal type", with: @entry.meal_type
     fill_in "Proteins", with: @entry.proteins
     click_on "Update Entry"
 
     assert_text "Entry was successfully updated"
-    click_on "Back"
   end
 
   test "destroying a Entry" do
